104c3d933ef8df3d63271e69295eda6c41b54118
lee sujin <jin18302@naver.com>
2025-07-24 15:45:29 +0900

    delete : 불필요한 도메인 제거

--- src/main/java/hairSalonReservation/sideProject/domain/calendar/repository/CalendarRepository.java
+++
@@ -1,7 +0,0 @@
-package hairSalonReservation.sideProject.domain.calendar.repository;
-
-import hairSalonReservation.sideProject.domain.calendar.entity.Calendar;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CalendarRepository extends JpaRepository<Calendar, Long> {
-}
--- src/main/java/hairSalonReservation/sideProject/domain/calendar/repository/SlotRepository.java
+++
@@ -1,7 +0,0 @@
-package hairSalonReservation.sideProject.domain.calendar.repository;
-
-import hairSalonReservation.sideProject.domain.calendar.entity.Slot;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SlotRepository extends JpaRepository<Slot, Long> {
-}
