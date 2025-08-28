75d4e6f06279f8930182db2863f09361331ac324
lee sujin <jin18302@naver.com>
2025-08-28 14:10:34 +0900

    add -리뷰 커스텀 레포지토리 생성 -커서페이지네이션 메서드 선언

--- src/main/java/hairSalonReservation/sideProject/domain/review/reposiory/ReviewCustomRepository.java
+++ src/main/java/hairSalonReservation/sideProject/domain/review/reposiory/ReviewCustomRepository.java
@@ -1,12 +1,13 @@
 package hairSalonReservation.sideProject.domain.review.reposiory;
 
+import hairSalonReservation.sideProject.common.enums.SortType;
 import hairSalonReservation.sideProject.domain.review.dto.response.ReviewResponse;
 
 import java.util.List;
 
 public interface ReviewCustomRepository {
 
-    List<ReviewResponse> findByShop(Long shopId, Long cursor);
+    List<ReviewResponse> findByShop(Long shopId, String cursor, SortType sortType);
 
-    List<ReviewResponse> findByDesigner(Long designerId, Long cursor);
+    List<ReviewResponse> findByDesigner(Long designerId, Long cursor, SortType sortType);
 }
