73a32cd0ca0ff5a7a5e6d24f20e38a60148dca93
lee sujin <jin18302@naver.com>
2025-08-28 14:04:49 +0900

    fix: 엔드포인트 수정

--- src/main/java/hairSalonReservation/sideProject/domain/review/controller/ReviewController.java
+++ src/main/java/hairSalonReservation/sideProject/domain/review/controller/ReviewController.java
@@ -17,50 +17,52 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
-    @PostMapping("reservations/{reservationId}")
+    @PostMapping("/reservations/{reservationId}/reviews")
     public ResponseEntity<ReviewResponse> createReview(
             @PathVariable(name = "reservationId") Long reservationId,
             @RequestAttribute("userId") Long userId,
             @RequestBody CreateReviewRequest createReviewRequest
-    ){
+    ) {
 
         ReviewResponse reviewResponse = reviewService.createReview(reservationId, userId, createReviewRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(reviewResponse);
     }
 
     @GetMapping("/shops/{shopId}/reviews")
-    public ResponseEntity<CursorPageResponse<ReviewResponse>> readByShop(@PathVariable(name = "shopId")Long shopId,
-                                                                         @RequestParam(name = "cursor", required = false) Long cursor
-    ){
+    public ResponseEntity<CursorPageResponse<ReviewResponse>> readByShop(@PathVariable(name = "shopId") Long shopId,
+                                                                         @RequestParam(name = "cursor", required = false) String cursor,
+                                                                         @RequestParam(name = "sort", required = false, defaultValue = "create_desc") String sort
+    ) {
 
-        CursorPageResponse<ReviewResponse> reviewResponseList = reviewService.readByShop(shopId,cursor);
+        CursorPageResponse<ReviewResponse> reviewResponseList = reviewService.readByShop(shopId, cursor, sort);
         return ResponseEntity.status(HttpStatus.OK).body(reviewResponseList);
     }
 
     @GetMapping("/designers/{designerId}/reviews")
-    public ResponseEntity<CursorPageResponse<ReviewResponse>> readByDesigner(@PathVariable(name = "designerId")Long designerId,
-                                                               @RequestParam(name = "cursor", required = false) Long cursor
-    ){
+    public ResponseEntity<CursorPageResponse<ReviewResponse>> readByDesigner(@PathVariable(name = "designerId") Long designerId,
+                                                                             @RequestParam(name = "cursor", required = false) Long cursor,
+                                                                             @RequestParam(name = "sort", required = false, defaultValue = "create_desc") String sort
+    ) {
 
-        CursorPageResponse<ReviewResponse> reviewResponseList = reviewService.readByDesigner(designerId, cursor);
+        CursorPageResponse<ReviewResponse> reviewResponseList = reviewService.readByDesigner(designerId, cursor, sort);
         return ResponseEntity.status(HttpStatus.OK).body(reviewResponseList);
     }
 
-    @PatchMapping("reservations/{reservationId}")
+    @PatchMapping("/reviews/{reviewId}")
     public ResponseEntity<ReviewResponse> updateReview(
-            @PathVariable(name = "reservationId") Long reservationId,
+            @PathVariable(name = "reviewId") Long reviewId,
             @RequestAttribute("userId") Long userId,
             @RequestBody UpdateReviewRequest updateReviewRequest
-    ){
-        ReviewResponse reviewResponse = reviewService.updateReview(reservationId, userId, updateReviewRequest);
+    ) {
+        ReviewResponse reviewResponse = reviewService.updateReview(reviewId, userId, updateReviewRequest);
         return ResponseEntity.status(HttpStatus.OK).body(reviewResponse);
     }
 
-    @DeleteMapping("reviews/{reviewId}")
+    @DeleteMapping("/reviews/{reviewId}")
     public ResponseEntity<Void> deleteReview(
             @PathVariable(name = "reviewId") Long reviewId,
             @RequestAttribute("userId") Long userId
-    ){
+    ) {
         reviewService.deleteReview(reviewId, userId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
