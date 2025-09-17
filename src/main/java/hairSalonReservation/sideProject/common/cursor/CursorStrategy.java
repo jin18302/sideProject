e261484b10dc9350f9bfc0aa1ab0376e687a6f99
jin18302 <jin18302@naver.com>
2025-09-17 16:17:35 +0900

    feat : ReviewCursorStrategy 생성 및 내부로직 구현

--- src/main/java/hairSalonReservation/sideProject/common/cursor/CursorStrategy.java
+++ src/main/java/hairSalonReservation/sideProject/common/cursor/CursorStrategy.java
@@ -1,8 +1,10 @@
 package hairSalonReservation.sideProject.common.cursor;
 
+import com.querydsl.core.types.Order;
 import com.querydsl.core.types.Predicate;
+import hairSalonReservation.sideProject.domain.review.entity.SortField;
 
 public interface CursorStrategy<T> {
 
-    Predicate buildCursorPredicate(T t, String cursor);
+    Predicate buildCursorPredicate(T t, String cursor, Order order, SortField sortField);
 }
---
+++ src/main/java/hairSalonReservation/sideProject/common/cursor/ReviewCursorStrategy.java
@@ -0,0 +1,45 @@
+package hairSalonReservation.sideProject.common.cursor;
+
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.StringExpression;
+import hairSalonReservation.sideProject.domain.review.entity.QReview;
+import hairSalonReservation.sideProject.domain.review.entity.SortField;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReviewCursorStrategy implements CursorStrategy<QReview>{
+
+    @Override
+    public Predicate buildCursorPredicate(QReview review, String lastCursor, Order order, SortField sortField) {
+
+        if(lastCursor == null){return Expressions.TRUE.isTrue(); }
+
+        StringExpression cursorExpr;
+
+        switch (sortField){
+            case CREATED_AT -> cursorExpr = createdAtExpression(review);
+            case RATING -> cursorExpr = ratingAtExpression(review);
+            default -> throw new RuntimeException();
+        }
+
+        switch (order){
+            case DESC -> {return cursorExpr.gt(lastCursor);}
+            case ASC -> {return cursorExpr.lt(lastCursor);}
+            default -> throw new RuntimeException();
+        }
+    }
+
+    private StringExpression createdAtExpression(QReview review){
+        return  Expressions.stringTemplate(
+                        "CONCAT(DATE_FORMAT({0}, '%Y%m%d%H%i%s%f'), LPAD({1}, 20, '0'))",
+                        review.createdAt, String.valueOf(review.id));
+    }
+
+    private StringExpression ratingAtExpression (QReview review) {
+        return Expressions.stringTemplate(
+                        "CONCAT({0}, LPAD({1}, 20, '0'))"
+                        ,review.rating, review.id);
+    }
+}
