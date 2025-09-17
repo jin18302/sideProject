89511f875e4b55b089ce8e7d6a287de20eb7d855
jin18302 <jin18302@naver.com>
2025-09-17 16:13:38 +0900

    chore : ReviewSortType 클래스 이름 SortField으로 변경

--- src/main/java/hairSalonReservation/sideProject/domain/review/entity/SortField.java
+++ src/main/java/hairSalonReservation/sideProject/domain/review/entity/SortField.java
@@ -3,7 +3,7 @@ package hairSalonReservation.sideProject.domain.review.entity;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public enum ReviewSortType {
+public enum SortField { //TODO: 엔티티마다 가능한 필드가 다를것이기때문에 이 부분 개선 팔요
     CREATED_AT,
     RATING;
 }
