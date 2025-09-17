3dfbeabcef32a4b95f87e0db3b1252e9ad031fc8
jin18302 <jin18302@naver.com>
2025-09-17 16:18:04 +0900

    chore : OrderSpecifierFactory 메서드 이름 변경

--- src/main/java/hairSalonReservation/sideProject/common/util/OrderSpecifierFactory.java
+++ src/main/java/hairSalonReservation/sideProject/common/util/OrderSpecifierFactory.java
@@ -3,9 +3,9 @@ package hairSalonReservation.sideProject.common.util;
 import com.querydsl.core.types.EntityPath;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
-import hairSalonReservation.sideProject.domain.review.entity.ReviewSortType;
+import hairSalonReservation.sideProject.domain.review.entity.SortField;
 
 public interface OrderSpecifierFactory<T extends EntityPath<?>> {
 
-     OrderSpecifier<?> temp(T t, ReviewSortType sortType, Order order);
+     OrderSpecifier<?> generateOrderSpecifier(T t, SortField sortType, Order order);
 }
