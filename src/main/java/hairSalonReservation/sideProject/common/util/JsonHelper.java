3adcefc4c638a8e64dec00457048661a0018bb6e
jin18302 <jin18302@naver.com>
2025-09-30 12:48:37 +0900

    chore: -json helper 클래스멤버 관련 설정 -예외처리 스택트레이스 출력

--- src/main/java/hairSalonReservation/sideProject/common/util/JsonHelper.java
+++ src/main/java/hairSalonReservation/sideProject/common/util/JsonHelper.java
@@ -3,30 +3,44 @@ package hairSalonReservation.sideProject.common.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
-@RequiredArgsConstructor
 public class JsonHelper {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final ObjectMapper objectMapper;
+
+    static {
+        objectMapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+
+    private JsonHelper(){}
+
+
 
     public static String toJson(Object object){
         try{
             return  objectMapper.writeValueAsString(object);
         }catch (JsonProcessingException e){
+            log.error("처리중 오류 발생",e);
             throw new RuntimeException("json 직렬화 중 알 수 없는 오류 발생");
         }
     }
 
     public static <T> T fromJson(String json, Class<T> tClass){
+
+
         try{
             return objectMapper.readValue(json, tClass);
         }catch (JsonProcessingException e){
+            log.error("처리중 오류 발생",e);
             throw new RuntimeException("json 역직렬화 중 알 수 없는 오류 발생");
         }
     }
@@ -38,7 +52,7 @@ public class JsonHelper {
         try{
             return objectMapper.readValue(json, typeReference);
         }catch (JsonProcessingException e){
-            log.error("원본 에러 이유 : {}", e.getMessage());
+            log.error("처리중 오류 발생",e);
             throw new RuntimeException("json 역직렬화 중 알 수 없는 오류 발생");
         }
     }
