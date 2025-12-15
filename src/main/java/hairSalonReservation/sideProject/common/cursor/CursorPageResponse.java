3be6a76ef869a827f4a80b4a7fd90db671f7e3f8
jin18302 <jin18302@naver.com>
2025-12-15 16:13:03 +0900

    fix: -CursorPageResponse 컨텐츠 null일 경우 발생하는 버그 픽스

--- src/main/java/hairSalonReservation/sideProject/common/cursor/CursorPageResponse.java
+++ src/main/java/hairSalonReservation/sideProject/common/cursor/CursorPageResponse.java
@@ -1,13 +1,19 @@
 package hairSalonReservation.sideProject.common.cursor;
 
 import hairSalonReservation.sideProject.common.config.QueryProperties;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.function.Function;
 
+@Slf4j
 public record CursorPageResponse<T>(List<T> content, Long lastCursor, boolean isLastPage) {
 
     public static <T> CursorPageResponse<T> of(List<T> contentList, Function<T, Long> idExtractor) { //
+
+        log.info("response:{}", contentList.size());
+
+        if(contentList.isEmpty()){return null;};
 
         boolean isLast = contentList.size() < new QueryProperties().getLimit() + 1;
         if (!isLast) {contentList.remove(contentList.size() - 1);}
