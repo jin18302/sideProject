a7aeaead7bd4b3d1055d5457cf478d50fb82caa0
jin18302 <jin18302@naver.com>
2025-12-20 11:36:11 +0900

    refactor: - res 불필요한 필드 제거

--- src/main/java/hairSalonReservation/sideProject/domain/serviceMenu/dto/response/ServiceMenuResponse.java
+++ src/main/java/hairSalonReservation/sideProject/domain/serviceMenu/dto/response/ServiceMenuResponse.java
@@ -1,11 +1,9 @@
 package hairSalonReservation.sideProject.domain.serviceMenu.dto.response;
 
-import hairSalonReservation.sideProject.domain.serviceMenu.entity.MenuCategory;
 import hairSalonReservation.sideProject.domain.serviceMenu.entity.ServiceMenu;
 
 public record ServiceMenuResponse(
         Long id,
-        MenuCategory category,
         String name,
         Integer price,
         String introduction
@@ -14,7 +12,6 @@ public record ServiceMenuResponse(
     public static ServiceMenuResponse from(ServiceMenu serviceMenu){
         return new ServiceMenuResponse(
                 serviceMenu.getId(),
-                serviceMenu.getCategory(),
                 serviceMenu.getName(),
                 serviceMenu.getPrice(),
                 serviceMenu.getIntroduction()
