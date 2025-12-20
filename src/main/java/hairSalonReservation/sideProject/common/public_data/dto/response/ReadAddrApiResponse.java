f36d610d77a6965d8c8383d476135125184b2339
jin18302 <jin18302@naver.com>
2025-12-20 11:33:46 +0900

    refactor: - 단계별 주소 조회 api respose 커스텀 생성자 정의 - 외부 api 로 받은 res와 반환 res의 필드명 매칭위한 @JsonAlias 적용

--- src/main/java/hairSalonReservation/sideProject/common/public_data/dto/response/ReadAddrApiResponse.java
+++ src/main/java/hairSalonReservation/sideProject/common/public_data/dto/response/ReadAddrApiResponse.java
@@ -1,6 +1,15 @@
 package hairSalonReservation.sideProject.common.public_data.dto.response;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record ReadAddrApiResponse(List<ReadAddrResponse> result) {
+
+    public ReadAddrApiResponse(List<ReadAddrResponse> result){
+
+        List<ReadAddrResponse> resultList = new ArrayList<>();
+        resultList.add(0, ReadAddrResponse.from("0", "전체", "전체"));
+        resultList.addAll(result);
+       this.result = resultList;
+    }
 }
--- src/main/java/hairSalonReservation/sideProject/common/public_data/dto/response/ReadAddrResponse.java
+++ src/main/java/hairSalonReservation/sideProject/common/public_data/dto/response/ReadAddrResponse.java
@@ -1,7 +1,11 @@
 package hairSalonReservation.sideProject.common.public_data.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonAlias;
 
-public record ReadAddrResponse(String cd, @JsonProperty("addr_name")String address, @JsonProperty("full_addr")String fullAddress) {
+public record ReadAddrResponse(String cd, @JsonAlias("addr_name")String address, @JsonAlias("full_addr")String fullAddress) {
+
+    public static ReadAddrResponse from(String cd, String address, String fullAddress){
+        return new ReadAddrResponse(cd, address, fullAddress);
+    }
 
 }
