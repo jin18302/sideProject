95b74f2b32bebd2d9a10544b07c6a54c5a4c58d4
jin18302 <jin18302@naver.com>
2025-12-20 11:35:34 +0900

    add: 스케줄링 위한 어노테이션 설정

--- src/main/java/hairSalonReservation/sideProject/SideProjectApplication.java
+++ src/main/java/hairSalonReservation/sideProject/SideProjectApplication.java
@@ -3,8 +3,12 @@ package hairSalonReservation.sideProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
+@EnableScheduling
+@EnableJpaRepositories
 @SpringBootApplication
 public class SideProjectApplication {
 
